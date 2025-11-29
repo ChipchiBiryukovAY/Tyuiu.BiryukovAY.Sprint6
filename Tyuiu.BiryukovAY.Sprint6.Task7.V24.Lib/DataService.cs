fb1ef374f9d834dcffaeb8facd6f5c4a7a06aa9c
Алексей Бирюков <ChipichiX@yandex.ru>
2025-11-29 18:19:06 +0700

    Task7

--- Tyuiu.BiryukovAY.Sprint6.Task7.V24.Lib/DataService.cs
+++ Tyuiu.BiryukovAY.Sprint6.Task7.V24.Lib/DataService.cs
@@ -8,26 +8,32 @@ namespace Tyuiu.BiryukovAY.Sprint6.Task7.V24.Lib
         {
             try
             {
-                string[] lines = File.ReadAllLines(path);
+                string[] lines = File.ReadAllLines(path)
+                    .Where(line => !string.IsNullOrWhiteSpace(line))
+                    .ToArray();
 
+                if (lines.Length == 0)
+                    return new int[0, 0];
 
+                string[] firstLineValues = lines[0].Split(',');
                 int rowCount = lines.Length;
-                int colCount = lines[0].Split(',').Length;
+                int colCount = firstLineValues.Length;
 
                 int[,] matrix = new int[rowCount, colCount];
 
                 for (int i = 0; i < rowCount; i++)
                 {
                     string[] values = lines[i].Split(',');
+
                     for (int j = 0; j < colCount; j++)
                     {
-                        if (int.TryParse(values[j].Trim(), out int value))
+                        if (j < values.Length && int.TryParse(values[j].Trim(), out int value))
                         {
                             matrix[i, j] = value;
                         }
                         else
                         {
-                            matrix[i, j] = 0;
+                            matrix[i, j] = 0; 
                         }
                     }
                 }
--- Tyuiu.BiryukovAY.Sprint6.Task7.V24.Test/DataServiceTest.cs
+++ Tyuiu.BiryukovAY.Sprint6.Task7.V24.Test/DataServiceTest.cs
@@ -12,18 +12,26 @@ namespace Tyuiu.BiryukovAY.Sprint6.Task7.V24.Test
 
             string testFilePath = "test_matrix.csv";
             File.WriteAllText(testFilePath,
-                "1,2,3,4,5\n" +
-                "6,7,8,9,10\n" +
-                "11,12,13,14,15\n" +
-                "16,17,18,19,20\n" +
-                "21,22,23,24,25");
+                "-19,-4,13,-11,-15,5,-12,20,3,-19\n" +
+                "-17,1,5,1,1,1,1,1,1,-5\n" +
+                "11,-11,-10,16,18,13,-17,-13,1,4\n" +
+                "2,-1,-17,16,4,-1,20,18,-5,-2\n" +
+                "-17,-13,2,-12,4,-16,10,-16,2,-7\n" +
+                "-6,4,-3,2,-4,2,-10,3,5,-19\n" +
+                "11,7,20,-19,1,18,-3,-18,2,11\n" +
+                "15,-14,-11,17,18,-18,2,18,19,1\n" +
+                "-1,-12,-5,10,18,13,9,3,-8,16\n" +
+                "9,-20,-12,9,15,-1,3,-11,6,-4");
 
             int[,] result = ds.GetMatrix(testFilePath);
 
-            Assert.AreEqual(5, result.GetLength(0)); 
-            Assert.AreEqual(5, result.GetLength(1)); 
-            Assert.AreEqual(1, result[0, 0]);
-            Assert.AreEqual(25, result[4, 4]);
+            Assert.AreEqual(10, result.GetLength(0)); 
+            Assert.AreEqual(10, result.GetLength(1)); 
+
+            Assert.AreEqual(-19, result[0, 0]);
+            Assert.AreEqual(-4, result[0, 1]);
+            Assert.AreEqual(-5, result[1, 9]);
+            Assert.AreEqual(-4, result[9, 9]);
 
             File.Delete(testFilePath);
         }
@@ -34,25 +42,53 @@ namespace Tyuiu.BiryukovAY.Sprint6.Task7.V24.Test
             DataService ds = new DataService();
 
             int[,] inputMatrix = {
-                {1, 2, 3, 4, 5},
-                {6, 7, 8, 9, 10},
-                {11, 12, 13, 14, 15},
-                {16, 17, 18, 19, 20},
-                {21, 22, 23, 24, 25}
+                {-19, -4, 13, -11, -15, 5, -12, 20, 3, -19},
+                {-17, 1, 5, 1, 1, 1, 1, 1, 1, -5},
+                {11, -11, -10, 16, 18, 13, -17, -13, 1, 4},
+                {2, -1, -17, 16, 4, -1, 20, 18, -5, -2},
+                {-17, -13, 2, -12, 4, -16, 10, -16, 2, -7},
+                {-6, 4, -3, 2, -4, 2, -10, 3, 5, -19},
+                {11, 7, 20, -19, 1, 18, -3, -18, 2, 11},
+                {15, -14, -11, 17, 18, -18, 2, 18, 19, 1},
+                {-1, -12, -5, 10, 18, 13, 9, 3, -8, 16},
+                {9, -20, -12, 9, 15, -1, 3, -11, 6, -4}
             };
 
             int[,] result = ds.ProcessMatrix(inputMatrix);
 
-            Assert.AreEqual(1, result[1, 0]);
-            Assert.AreEqual(7, result[1, 1]); 
-            Assert.AreEqual(1, result[1, 2]); 
-            Assert.AreEqual(9, result[1, 3]); 
-            Assert.AreEqual(1, result[1, 4]); 
 
-            Assert.AreEqual(1, result[0, 0]);
+            Assert.AreEqual(-17, result[1, 0]);
+            Assert.AreEqual(1, result[1, 1]);
+            Assert.AreEqual(5, result[1, 2]);
+            Assert.AreEqual(1, result[1, 3]);
+            Assert.AreEqual(1, result[1, 4]);
+            Assert.AreEqual(1, result[1, 5]);
+            Assert.AreEqual(1, result[1, 6]);
+            Assert.AreEqual(1, result[1, 7]);
+            Assert.AreEqual(1, result[1, 8]);
+            Assert.AreEqual(-5, result[1, 9]);
+
+            Assert.AreEqual(-19, result[0, 0]);
             Assert.AreEqual(11, result[2, 0]);
-            Assert.AreEqual(16, result[3, 0]);
-            Assert.AreEqual(21, result[4, 0]);
+            Assert.AreEqual(9, result[9, 0]);
+        }
+
+        [TestMethod]
+        public void ProcessMatrixWithEvenNumbers()
+        {
+            DataService ds = new DataService();
+
+            int[,] inputMatrix = {
+                {1, 2, 3},
+                {4, 5, 6}, 
+                {7, 8, 9}
+            };
+
+            int[,] result = ds.ProcessMatrix(inputMatrix);
+
+            Assert.AreEqual(1, result[1, 0]); 
+            Assert.AreEqual(5, result[1, 1]); 
+            Assert.AreEqual(1, result[1, 2]); 
         }
 
         [TestMethod]
