b1310cb5ff23f522278f97eedf7ebd4f2524db77
Алексей Бирюков <ChipichiX@yandex.ru>
2025-11-29 18:26:02 +0700

    Task7

--- Tyuiu.BiryukovAY.Sprint6.Task7.V24.Lib/DataService.cs
+++ Tyuiu.BiryukovAY.Sprint6.Task7.V24.Lib/DataService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using tyuiu.cources.programming.interfaces.Sprint6;
 namespace Tyuiu.BiryukovAY.Sprint6.Task7.V24.Lib
 {
@@ -8,32 +9,48 @@ namespace Tyuiu.BiryukovAY.Sprint6.Task7.V24.Lib
         {
             try
             {
-                string[] lines = File.ReadAllLines(path)
-                    .Where(line => !string.IsNullOrWhiteSpace(line))
-                    .ToArray();
+                string[] allLines = File.ReadAllLines(path);
 
-                if (lines.Length == 0)
+                List<string> validLines = new List<string>();
+                foreach (string line in allLines)
+                {
+                    string trimmedLine = line.Trim();
+                    if (!string.IsNullOrEmpty(trimmedLine))
+                    {
+                        validLines.Add(trimmedLine);
+                    }
+                }
+
+                if (validLines.Count == 0)
                     return new int[0, 0];
 
-                string[] firstLineValues = lines[0].Split(',');
-                int rowCount = lines.Length;
+                string[] firstLineValues = ParseCSVLine(validLines[0]);
+                int rowCount = validLines.Count;
                 int colCount = firstLineValues.Length;
 
                 int[,] matrix = new int[rowCount, colCount];
 
                 for (int i = 0; i < rowCount; i++)
                 {
-                    string[] values = lines[i].Split(',');
+                    string[] values = ParseCSVLine(validLines[i]);
 
                     for (int j = 0; j < colCount; j++)
                     {
-                        if (j < values.Length && int.TryParse(values[j].Trim(), out int value))
+                        if (j < values.Length)
                         {
-                            matrix[i, j] = value;
+                            string cleanValue = values[j].Trim();
+                            if (int.TryParse(cleanValue, NumberStyles.Integer, CultureInfo.InvariantCulture, out int value))
+                            {
+                                matrix[i, j] = value;
+                            }
+                            else
+                            {
+                                matrix[i, j] = 0;
+                            }
                         }
                         else
                         {
-                            matrix[i, j] = 0; 
+                            matrix[i, j] = 0;
                         }
                     }
                 }
@@ -44,6 +61,11 @@ namespace Tyuiu.BiryukovAY.Sprint6.Task7.V24.Lib
             {
                 throw new Exception($"Ошибка чтения файла: {ex.Message}");
             }
+        }
+
+        private string[] ParseCSVLine(string line)
+        {
+            return line.Split(',');
         }
 
         public int[,] ProcessMatrix(int[,] matrix)
